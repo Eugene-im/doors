61e4c6d71c5af13d335b4d2a752f099a7683c9d8
Eugene-im <em.92@mail.ru>
2018-09-20 17:02:23 +0300

    add

--- js/main.js
+++ js/main.js
@@ -1,1 +1,5 @@
 $(".header__slider").slick({});
+$(".galery .article__slider").slick({});
+$(".tab_mass .tab__slider").slick({});
+$(".tab_shpon .tab__slider").slick({});
+$(".tab_eco-shpon .tab__slider").slick({});
