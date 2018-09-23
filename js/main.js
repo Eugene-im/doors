fc2a9303e15f17a379e313d09174a4235a3e3134
Eugene-im <em.92@mail.ru>
2018-09-23 23:28:25 +0300

    add

--- js/main.js
+++ js/main.js
@@ -9,3 +9,13 @@ $(".galery .article__slider").slick({
 $(".tab_mass.tab__slider").slick({});
 $(".tab_shpon.tab__slider").slick({});
 $(".tab_eco-shpon.tab__slider").slick({});
+$(".furnitura .article__slider").slick({
+    arrows:false,
+    dots: true
+});
+$(".furnitura .furnitura__brand").slick({
+    arrows:false,
+    dots: false,
+    slidesToShow: 15,
+    slidesToScroll: 3
+});
