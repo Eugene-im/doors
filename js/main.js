cb4eee17c6ed8969e96522d345e245af91c22d5b
Eugene-im <em.92@mail.ru>
2018-09-23 18:55:31 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 $(".header__slider").slick({});
-$(".galery .article__slider").slick({});
-$(".tab_mass .tab__slider").slick({});
-$(".tab_shpon .tab__slider").slick({});
-$(".tab_eco-shpon .tab__slider").slick({});
+$(".galery .article__slider").slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    dots: true,
+    arrows: false
+});
+$(".tab_mass.tab__slider").slick({});
+$(".tab_shpon.tab__slider").slick({});
+$(".tab_eco-shpon.tab__slider").slick({});
